47d0fdfc10ced8ccfa8b039789ae1aacafc6a16a
Shahzada Shoaib <124442653+Shoaib-bit@users.noreply.github.com>
2025-10-04 11:51:38 +0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div>
+        <p>Hello World</p>
+
+        <Button>Click Me</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
